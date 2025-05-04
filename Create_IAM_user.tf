8b260e03078885c6409a14b1ec2760c048f33de3
Ayush <ayush.pandey89999@gmail.com>
2025-05-04 10:00:14 +0530

    Adding Create user file

---
+++ Create_IAM_user.tf
@@ -0,0 +1,49 @@
+# Configuring AWS
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Configuring AWS provider
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+# Creating user
+resource "aws_iam_user" "terraform_user" {
+  name = "terraform_user"
+}
+
+# OPTIONAL!! Creating user access key for login using CLI 
+resource "aws_iam_access_key" "terraform_user_access_key" {
+  user = aws_iam_user.terraform_user.name
+}
+
+# Getting secret key
+output "secret_access_key" {
+  value     = aws_iam_access_key.terraform_user_access_key.secret
+  sensitive = true
+}
+
+# Creating policy for user
+data "aws_iam_policy_document" "Create_Terraform_user_policy" {
+  statement {
+    effect    = "Allow"
+    actions   = ["*"]
+    resources = ["*"]
+  }
+}
+
+# Attching Policy to user
+resource "aws_iam_user_policy" "Assign_terraform_user_policy" {
+  name   = "terraform_user_policy"
+  user   = aws_iam_user.terraform_user.name
+  policy = data.aws_iam_policy_document.Create_Terraform_user_policy.json
+
+}
